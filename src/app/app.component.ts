7da595fd2614477021b6ed59a4e41224845d8a38
Lulu Khaira <lulukxhaira@gmail.com~>
2025-02-17 09:00:07 +0700

    masi error

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,21 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { LoginComponent } from "./menu/login/login.component";
+import { SignupComponent } from "./menu/signup/signup.component";
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [CommonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'angular19Test';
+  
+  x = 7;
+  y = 19;
+
+  user1 = "sauma";
+  user2 = "lulu";
 }
