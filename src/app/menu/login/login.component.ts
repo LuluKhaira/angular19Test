c0f90f70381c880fa05d589835c97d7be1041fa9
Lulu Khaira <lulukxhaira@gmail.com~>
2025-02-18 14:14:57 +0700

    entah

--- src/app/menu/login/login.component.ts
+++ src/app/menu/login/login.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+
 
 @Component({
   selector: 'app-login',
@@ -6,6 +7,10 @@ import { Component } from '@angular/core';
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
-export class LoginComponent {
+export class LoginComponent implements OnInit {
+
+  ngOnInit() {
+    console.log("LoginComponent dimuat");
+  }
 
 }
